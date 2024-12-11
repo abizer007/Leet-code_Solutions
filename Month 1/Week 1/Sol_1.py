3af0133bdf5941ef7890e6a0d7677813badea84e
abizer007 <133958757+abizer007@users.noreply.github.com>
2024-12-11 11:00:55 +0530

    Create Sol_1.py

---
+++ Month 1/Week 1/Sol_1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_to_index = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+
+            num_to_index[num] = i
+
+if __name__ == "__main__":
+    solution = Solution()
+
+        
